c1d5158756bc9a403d2f49954eb8dc1b99390f2a
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-03-18 18:39:56 +0200

    Empty Project Files

---
+++ PomodoroTimer/Shared/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  Shared
+//
+//  Created by Marina on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
